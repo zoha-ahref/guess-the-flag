3e528216bfbe89127320212af9416acf3665e92f
zoha <sweetangelzoha@gmail.com>
2021-01-20 12:01:15 -0500

    Files to commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Zoha Kaukab on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
